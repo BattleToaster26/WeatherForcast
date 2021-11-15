a2f1c0cd758cae3999f43eb6a67a3450cdab6802
BattleToaster26 <zcbaltzer@gmail.com>
2021-11-15 13:05:50 -0600

    API file is done

---
+++ WeatherForcast/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherForcast
+//
+//  Created by PMStudent on 11/15/21.
+//
+
+import Foundation
+
+struct API {
+    static let key = "6468b96f67741c264c12a1290c0c8a6c"
+}
