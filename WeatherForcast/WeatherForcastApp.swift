0da09f48dfa6a61fccc315cad1fe3fa02ea9426a
BattleToaster26 <zcbaltzer@gmail.com>
2021-11-15 12:40:43 -0600

    Initial Commit

---
+++ WeatherForcast/WeatherForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastApp.swift
+//  WeatherForcast
+//
+//  Created by PMStudent on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
